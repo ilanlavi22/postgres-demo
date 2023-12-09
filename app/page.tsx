2232c25f5a3e324263f7eae528a59ac7a885f3ec
ilanlavi <refaelberlin@gmail.com>
2023-12-10 00:44:28 +0100

    initial P.SDK

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import postgres from "postgres";
+//import postgres from "postgres";
 
-const sql = postgres(process.env.POSTGRES_URL!);
+// const sql = postgres(process.env.POSTGRES_URL!);
 
-async function getPosts() {
-  const posts = await sql`SELECT * FROM posts`;
-  console.log(posts);
+// async function getPosts() {
+//   const posts = await sql`SELECT * FROM posts`;
+//   console.log(posts);
 
-  return posts;
-}
+//   return posts;
+// }
 
 export default async function Home() {
-  const allPosts = await getPosts();
+  //const allPosts = await getPosts();
   return (
     <section>
       <h1>Home</h1>
